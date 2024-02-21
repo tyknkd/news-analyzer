fa3eb077dd0c1843a4ab739271a461a9df048fb6
tyknkd <78797823+tyknkd@users.noreply.github.com>
2024-02-21 11:18:52 +0900

    test: add endpoint tests

---
+++ applications/data-collector/src/test/kotlin/test/newsanalyzer/datacollector/ApplicationTest.kt
@@ -0,0 +1,41 @@
+package test.newsanalyzer.datacollector
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+import io.newsanalyzer.datacollector.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.PermanentRedirect, status)
+        }
+    }
+    @Test
+    fun testHealth() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/health").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("OK", bodyAsText())
+        }
+    }
+    @Test
+    fun testArticles() = testApplication {
+        application {
+            configureSerialization()
+            configureDatabases()
+            configureRouting()
+        }
+        client.get("/articles").apply {
+            assertEquals(HttpStatusCode.OK, status)
+        }
+    }
+}
