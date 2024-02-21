0815885ef9fda258b2107b0af931451b5300201d
tyknkd <78797823+tyknkd@users.noreply.github.com>
2024-02-21 15:52:34 +0900

    refactor: pretty print json

--- applications/data-collector/src/main/kotlin/io/newsanalyzer/datacollector/plugins/Serialization.kt
+++ applications/data-collector/src/main/kotlin/io/newsanalyzer/datacollector/plugins/Serialization.kt
@@ -4,10 +4,13 @@ import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.json.Json
 
 fun Application.configureSerialization() {
     install(ContentNegotiation) {
-        json()
+        json(Json {
+            prettyPrint = true
+        })
     }
     routing {
     }
