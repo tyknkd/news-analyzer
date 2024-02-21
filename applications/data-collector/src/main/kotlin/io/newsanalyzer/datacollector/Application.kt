676a04052ea11c8d14fabe62d52e674e19b6ad79
tyknkd <78797823+tyknkd@users.noreply.github.com>
2024-02-21 10:58:15 +0900

    refactor: set timezone to UTC

--- applications/data-collector/src/main/kotlin/io/newsanalyzer/datacollector/Application.kt
+++ applications/data-collector/src/main/kotlin/io/newsanalyzer/datacollector/Application.kt
@@ -3,12 +3,11 @@ package io.newsanalyzer.datacollector
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import java.util.*
 import io.newsanalyzer.datacollector.plugins.*
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-
 
 fun main() {
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
     val port = System.getenv("COLLECTOR_PORT")?.toInt() ?: 8081
     embeddedServer(factory = Netty, port = port, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
