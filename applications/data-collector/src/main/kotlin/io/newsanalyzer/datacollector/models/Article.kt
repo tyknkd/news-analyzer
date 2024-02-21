e2807b34135a6fcf800c8ff22d915181b4952ab9
tyknkd <78797823+tyknkd@users.noreply.github.com>
2024-02-21 15:53:53 +0900

    refactor: modularize article models

---
+++ applications/data-collector/src/main/kotlin/io/newsanalyzer/datacollector/models/Article.kt
@@ -0,0 +1,17 @@
+package io.newsanalyzer.datacollector.models
+
+import kotlinx.datetime.Instant
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Article(
+    val id: Int,
+    val publisher: String,
+    val author: String,
+    val title: String,
+    val description: String,
+    val url: String,
+    val urlToImage: String,
+    val publishedAt: Instant,
+    val content: String
+)
--- applications/data-collector/src/main/kotlin/io/newsanalyzer/datacollector/models/Articles.kt
+++ applications/data-collector/src/main/kotlin/io/newsanalyzer/datacollector/models/Articles.kt
@@ -17,16 +17,3 @@ object Articles: Table() {
     val content = varchar("content", 512)
     override val primaryKey = PrimaryKey(id)
 }
-
-@Serializable
-data class Article(
-    val id: Int,
-    val publisher: String,
-    val author: String,
-    val title: String,
-    val description: String,
-    val url: String,
-    val urlToImage: String,
-    val publishedAt: Instant,
-    val content: String
-)
