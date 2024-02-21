8702e246303ca2271c8a4ad654a04e5a5b2e047e
tyknkd <78797823+tyknkd@users.noreply.github.com>
2024-02-21 11:25:18 +0900

    refactor: set webserver timezone to UTC

--- applications/web-server/src/main/kotlin/io/newsanalyzer/webserver/Application.kt
+++ applications/web-server/src/main/kotlin/io/newsanalyzer/webserver/Application.kt
@@ -4,8 +4,10 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.newsanalyzer.webserver.plugins.*
+import java.util.*
 
 fun main() {
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
     val port = System.getenv("WEBSERVER_PORT")?.toInt() ?: 8080
     embeddedServer(factory = Netty, port = port, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
