950660e2c922e73298ddba6c88af1122b3fd69b5
tyknkd <78797823+tyknkd@users.noreply.github.com>
2024-02-22 18:45:04 +0900

    test: init server once before all tests

--- applications/data-collector/src/test/kotlin/test/newsanalyzer/datacollector/ApplicationTest.kt
+++ applications/data-collector/src/test/kotlin/test/newsanalyzer/datacollector/ApplicationTest.kt
@@ -1,43 +1,67 @@
 package test.newsanalyzer.datacollector
 
+import io.newsanalyzer.datacollector.plugins.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
-import kotlin.test.*
-import io.newsanalyzer.datacollector.plugins.*
+import io.ktor.test.dispatcher.*
+import org.junit.Test
+import org.junit.BeforeClass
+import org.junit.AfterClass
+import kotlin.test.assertEquals
+import kotlin.test.assertContains
+import kotlin.test.assertNotEquals
+import kotlin.test.assertNotNull
 
 class ApplicationTest {
+    val apiKeyErrorMessage = "NEWS_API_KEY environment variable is invalid or not set. Check your key, or obtain a free key from https://newsapi.org"
     @Test
-    fun testRoot() = testApplication {
-        application {
-            configureSerialization()
-            configureDatabases()
-            configureRouting()
-        }
-        client.get("/").apply {
-            assertEquals(HttpStatusCode.OK, status)
+    fun testApiKey() {
+        val newsApiKey = System.getenv("NEWS_API_KEY")
+        assertNotNull(newsApiKey, apiKeyErrorMessage)
+        assertNotEquals(newsApiKey,"0123456789abcdefghijklmnopqrstuv", apiKeyErrorMessage)
+    }
+    @Test
+    fun testRoot() = testSuspend {
+        testApp.client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status, apiKeyErrorMessage)
+            assertContains(bodyAsText(), "\"publishedAt\":")
         }
     }
     @Test
-    fun testHealth() = testApplication {
-        application {
-            configureRouting()
-        }
-        client.get("/health").apply {
+    fun testHealth() = testSuspend {
+        testApp.client.get("/health").apply {
             assertEquals(HttpStatusCode.OK, status)
             assertEquals("OK", bodyAsText())
         }
     }
     @Test
-    fun testArticles() = testApplication {
-        application {
-            configureSerialization()
-            configureDatabases()
-            configureRouting()
+    fun testArticles() = testSuspend {
+        testApp.client.get("/articles").apply {
+            assertEquals(HttpStatusCode.OK, status, apiKeyErrorMessage)
+            assertContains(bodyAsText(), "\"publishedAt\":")
         }
-        client.get("/articles").apply {
-            assertEquals(HttpStatusCode.OK, status)
+    }
+
+    companion object {
+        lateinit var testApp: TestApplication
+        @JvmStatic
+        @BeforeClass
+        fun setup(): Unit {
+            testApp = TestApplication {
+                application {
+                    configureSerialization()
+                    configureDatabases()
+                    configureRouting()
+                }
+            }
+        }
+
+        @JvmStatic
+        @AfterClass
+        fun teardown() {
+            testApp.stop()
         }
     }
 }
