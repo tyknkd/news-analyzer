ded8ae7260a78df80ff5f38af0a3b81dd88736d4
tyknkd <78797823+tyknkd@users.noreply.github.com>
2024-02-23 15:21:39 +0900

    test: start webserver only once before all tests

--- applications/web-server/src/test/kotlin/test/newsanalyzer/webserver/ApplicationTest.kt
+++ applications/web-server/src/test/kotlin/test/newsanalyzer/webserver/ApplicationTest.kt
@@ -1,60 +1,69 @@
 package test.newsanalyzer.webserver
 
+import io.newsanalyzer.webserver.plugins.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
-import kotlin.test.*
-import io.newsanalyzer.webserver.plugins.*
+import io.ktor.test.dispatcher.*
+import org.junit.Test
+import org.junit.BeforeClass
+import org.junit.AfterClass
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
-    fun testRoot() = testApplication {
-        application {
-            configureTemplating()
-            configureRouting()
-        }
-        client.get("/").apply {
+    fun testRoot() = testSuspend {
+        testApp.client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
         }
     }
     @Test
-    fun testHealth() = testApplication {
-        application {
-            configureRouting()
-        }
-        client.get("/health").apply {
+    fun testHealth() = testSuspend {
+        testApp.client.get("/health").apply {
             assertEquals(HttpStatusCode.OK, status)
             assertEquals("OK", bodyAsText())
         }
     }
     @Test
-    fun testAPI() = testApplication {
-        application {
-            configureRouting()
-        }
-        client.get("/api").apply {
+    fun testAPI() = testSuspend {
+        testApp.client.get("/api").apply {
             assertEquals(HttpStatusCode.OK, status)
             assertEquals("News Analyzer API", bodyAsText())
         }
     }
     @Test
-    fun testStatic() = testApplication {
-        application {
-            configureRouting()
-        }
-        client.get("/static").apply {
+    fun testStatic() = testSuspend {
+        testApp.client.get("/static").apply {
             assertEquals(HttpStatusCode.OK, status)
         }
     }
     @Test
-    fun testArticles() = testApplication {
-        application {
-            configureTemplating()
-            configureRouting()
-        }
-        client.get("/articles/0").apply {
+    fun testArticles() = testSuspend {
+        testApp.client.get("/articles/0").apply {
             assertEquals(HttpStatusCode.OK, status)
+        }
+    }
+
+    companion object {
+        lateinit var testApp: TestApplication
+        @JvmStatic
+        @BeforeClass
+        fun setup(): Unit {
+            testApp = TestApplication {
+                application {
+                    configureMonitoring()
+                    configureSerialization()
+                    configureTemplating()
+                    configureRouting()
+                }
+            }
+        }
+
+        @JvmStatic
+        @AfterClass
+        fun teardown() {
+            testApp.stop()
         }
     }
 }
