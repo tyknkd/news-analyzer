b0361ac55bf31829f46f17bedbc6a14615882c3c
tyknkd <78797823+tyknkd@users.noreply.github.com>
2024-02-24 18:11:39 +0900

    refactor: remove h2 db user/pass

--- applications/data-collector/src/main/kotlin/io/newsanalyzer/datacollector/plugins/ArticlesDatabase.kt
+++ applications/data-collector/src/main/kotlin/io/newsanalyzer/datacollector/plugins/ArticlesDatabase.kt
@@ -12,27 +12,16 @@ object ArticlesDatabase {
     fun init() {
         val jdbcUrl = "jdbc:h2:file:./build/db"
         val driverClassName = "org.h2.Driver"
-        val user = "root"
-        val password = "root"
-//        val database = Database.connect(
-//            url = jdbcUrl,
-//            user = user,
-//            driver = driverClassName,
-//            password = password
-//        )
-        val database = Database.connect(createHikariDataSource(url = jdbcUrl, driver = driverClassName, user = user, pass = password))
+        val database = Database.connect(createHikariDataSource(url = jdbcUrl, driver = driverClassName))
         transaction(database) {
             SchemaUtils.create(Articles)
         }
     }
 
-    private fun createHikariDataSource(
-        url: String, driver: String, user: String, pass: String) =
+    private fun createHikariDataSource(url: String, driver: String) =
         HikariDataSource(HikariConfig().apply {
             jdbcUrl = url
             driverClassName = driver
-            username = user
-            password = pass
             maximumPoolSize = 3
             isAutoCommit = false
             transactionIsolation = "TRANSACTION_REPEATABLE_READ"
