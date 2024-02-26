3bb76d3e590db15706fe165a24f92461c21984b1
tyknkd <78797823+tyknkd@users.noreply.github.com>
2024-02-26 09:10:48 +0900

    refactor: use postgres_jdbc_version in gradle properties for clarity

--- buildSrc/src/main/kotlin/news-analyzer.kotlin-dataserver-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/news-analyzer.kotlin-dataserver-conventions.gradle.kts
@@ -5,13 +5,13 @@ plugins {
     id("news-analyzer.kotlin-application-conventions")
 }
 
-val postgres_version: String by project
+val postgres_jdbc_version: String by project
 val h2_version: String by project
 val exposed_version: String by project
 val hikaricp_version: String by project
 
 dependencies {
-    implementation("org.postgresql:postgresql:$postgres_version")
+    implementation("org.postgresql:postgresql:$postgres_jdbc_version")
     implementation("com.h2database:h2:$h2_version")
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
