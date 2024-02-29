4f59b4943abca29a40f401d5eb34c085a2ec6aae
tyknkd <78797823+tyknkd@users.noreply.github.com>
2024-02-29 20:01:00 +0900

    test: handle container env

--- applications/data-collector/src/test/kotlin/test/newsanalyzer/datacollector/ApplicationTest.kt
+++ applications/data-collector/src/test/kotlin/test/newsanalyzer/datacollector/ApplicationTest.kt
@@ -9,22 +9,31 @@ import io.ktor.test.dispatcher.*
 import org.junit.Test
 import org.junit.BeforeClass
 import org.junit.AfterClass
+import org.junit.FixMethodOrder
+import org.junit.runners.MethodSorters
+import java.nio.file.Files
+import java.nio.file.Paths
 import kotlin.test.assertEquals
 import kotlin.test.assertContains
 import kotlin.test.assertNotEquals
 import kotlin.test.assertNotNull
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class ApplicationTest {
-    val apiKeyErrorMessage = "NEWS_API_KEY environment variable is invalid or not set. Check your key, or obtain a free key from https://newsapi.org"
+    private val apiKeyErrorMessage = "NEWS_API_KEY environment variable is invalid or not set. Check your key, or obtain a free key from https://newsapi.org"
     @Test
     fun testApiKey() {
-        val newsApiKey = System.getenv("NEWS_API_KEY")
+        val newsApiKey = if (System.getenv("OS_ENV") == "container") {
+            Files.readAllBytes(Paths.get(System.getenv("NEWS_API_KEY_FILE"))).toString()
+        } else {
+            System.getenv("NEWS_API_KEY")
+        }
         assertNotNull(newsApiKey, apiKeyErrorMessage)
         assertNotEquals(newsApiKey,"yournewsapikeygoeshere", apiKeyErrorMessage)
     }
     @Test
-    fun testRoot() = testSuspend {
-        testApp.client.get("/").apply {
+    fun testArticles() = testSuspend {
+        testApp.client.get("/articles").apply {
             assertEquals(HttpStatusCode.OK, status, apiKeyErrorMessage)
             assertContains(bodyAsText(), "\"publishedAt\":")
         }
@@ -37,18 +46,19 @@ class ApplicationTest {
         }
     }
     @Test
-    fun testArticles() = testSuspend {
-        testApp.client.get("/articles").apply {
+    fun testRoot() = testSuspend {
+        testApp.client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status, apiKeyErrorMessage)
             assertContains(bodyAsText(), "\"publishedAt\":")
         }
     }
 
+
     companion object {
         lateinit var testApp: TestApplication
         @JvmStatic
         @BeforeClass
-        fun setup(): Unit {
+        fun setup() {
             testApp = TestApplication {
                 application {
                     configureSerialization()
