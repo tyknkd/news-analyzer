fbf0fb94f929529549d44e88f4eb1493f48c8c7e
tyknkd <78797823+tyknkd@users.noreply.github.com>
2024-03-03 17:31:20 +0900

    build: add Kt dataframe dep

--- buildSrc/src/main/kotlin/news-analyzer.kotlin-dataanalyzer-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/news-analyzer.kotlin-dataanalyzer-conventions.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 val spark_version: String by project
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:dataframe:0.13.0")
     implementation("org.jetbrains.kotlinx.spark:kotlin-spark-api_3.3.2_2.13:1.2.4")
     implementation("org.apache.spark:spark-sql_2.13:$spark_version")
     implementation("org.apache.spark:spark-mllib_2.13:$spark_version")
