2899092d3c23c508c8f3509c3e76d7c219ea604e
tyknkd <78797823+tyknkd@users.noreply.github.com>
2024-03-03 16:57:55 +0900

    build: add apache spark dependencies

---
+++ buildSrc/src/main/kotlin/news-analyzer.kotlin-dataanalyzer-conventions.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("news-analyzer.kotlin-dataserver-conventions")
+    id("com.github.johnrengelman.shadow")
+}
+
+val spark_version: String by project
+
+dependencies {
+    implementation("org.jetbrains.kotlinx.spark:kotlin-spark-api_3.3.2_2.13:1.2.4")
+    implementation("org.apache.spark:spark-sql_2.13:$spark_version")
+    implementation("org.apache.spark:spark-mllib_2.13:$spark_version")
+}
+
+tasks {
+    shadowJar {
+        configurations.forEach {
+            it.exclude("org.apache.spark")
+            it.exclude("org.scala-lang")
+        }
+    }
+}
