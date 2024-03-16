2fa6bf2a08aa8bdefa81578dfde14c90230818ce
tyknkd <78797823+tyknkd@users.noreply.github.com>
2024-03-17 08:11:09 +0900

    build: give all apps access to postgres pass

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
+# Extract Docker secret file to app environment
+export POSTGRES_PASSWORD=${POSTGRES_PASSWORD:=$(cat ${POSTGRES_PASSWORD_FILE})}
+
 if [ "${APP}" = "data-collector" ]; then
-  # Extract Docker secret files to app environment
-  export POSTGRES_PASSWORD=${POSTGRES_PASSWORD:=$(cat ${POSTGRES_PASSWORD_FILE})}
   export NEWS_API_KEY=${NEWS_API_KEY:=$(cat ${NEWS_API_KEY_FILE})}
 fi
 
