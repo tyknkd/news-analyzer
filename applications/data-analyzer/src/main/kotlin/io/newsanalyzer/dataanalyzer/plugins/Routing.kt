9dbd9d525cf6a00d4efa649a5f299b3d4b26265c
tyknkd <78797823+tyknkd@users.noreply.github.com>
2024-03-21 18:44:46 +0900

    feat: api post articles endpoint

--- applications/data-analyzer/src/main/kotlin/io/newsanalyzer/dataanalyzer/plugins/Routing.kt
+++ applications/data-analyzer/src/main/kotlin/io/newsanalyzer/dataanalyzer/plugins/Routing.kt
@@ -3,10 +3,13 @@ package io.newsanalyzer.dataanalyzer.plugins
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.request.*
 import io.ktor.server.resources.Resources
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.newsanalyzer.dataanalyzer.plugins.database.AnalyzedDataGateway
+import io.newsanalyzer.dataanalyzer.models.Article
+import io.newsanalyzer.dataanalyzer.plugins.database.RawDataGateway
 
 fun Application.configureRouting() {
     install(Resources)
@@ -26,6 +29,11 @@ fun Application.configureRouting() {
         get("/articles") {
             val articles = AnalyzedDataGateway.allArticles()
             call.respond(status = HttpStatusCode.OK, articles)
+        }
+        post("/articles") {
+            val articles = call.receive<List<Article>>()
+            RawDataGateway.addArticles(articles)
+            call.respondText("Articles received", status = HttpStatusCode.OK)
         }
         get("/health") {
             call.respondText(text = "OK", status = HttpStatusCode.OK)
