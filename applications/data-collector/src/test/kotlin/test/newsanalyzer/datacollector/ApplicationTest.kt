2b53840f0c56de9f227299d0b785c34b33ed012f
tyknkd <78797823+tyknkd@users.noreply.github.com>
2024-03-29 18:11:17 +0900

    test: fix: mock external services

--- applications/data-collector/src/test/kotlin/test/newsanalyzer/datacollector/ApplicationTest.kt
+++ applications/data-collector/src/test/kotlin/test/newsanalyzer/datacollector/ApplicationTest.kt
@@ -1,33 +1,32 @@
 package test.newsanalyzer.datacollector
 
+import io.ktor.client.call.*
 import io.newsanalyzer.datacollector.plugins.*
 import io.newsanalyzer.testsupport.TestDoubles
+import io.newsanalyzer.datasupport.models.*
+import io.newsanalyzer.httpsupport.HostPaths
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.testing.*
-import io.ktor.test.dispatcher.*
-import io.newsanalyzer.datasupport.models.RawArticles
-import io.newsanalyzer.httpsupport.*
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.Test
-import org.junit.BeforeClass
-import org.junit.AfterClass
 import org.junit.FixMethodOrder
 import org.junit.runners.MethodSorters
 import kotlin.test.assertEquals
-import kotlin.test.assertContains
 import kotlin.test.assertNotEquals
 import kotlin.test.assertNotNull
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class ApplicationTest {
     private val apiKeyErrorMessage = "NEWS_API_KEY environment variable is invalid or not set. Check your key, or obtain a free key from https://newsapi.org"
+
     @Test
     fun testApiKey() {
         val newsApiKey = System.getenv("NEWS_API_KEY")
@@ -35,83 +34,137 @@ class ApplicationTest {
         assertNotEquals(newsApiKey,"yournewsapikeygoeshere", apiKeyErrorMessage)
     }
     @Test
-    fun testArticles() = testSuspend {
-        testApp.client.get("/articles").apply {
-            assertEquals(HttpStatusCode.OK, status, apiKeyErrorMessage)
-            assertContains(bodyAsText(), "\"publishedAt\":")
+    fun testArticles() = testApplication {
+        lateinit var database: Database
+        val tables: List<Table> = listOf(RawArticles)
+        externalServices {
+            hosts("https://newsapi.org") {
+                install(ContentNegotiation) { json() }
+                routing {
+                    get("v2/everything{params}") {
+                        call.respond(status = HttpStatusCode.OK, TestDoubles.remoteData)
+                    }
+                }
+            }
+            val apiHost = HostPaths().getAnalyzerPath()
+            hosts("http://${apiHost}") {
+                install(ContentNegotiation) { json() }
+                routing {
+                    post("articles") {
+                        call.respondText("Updated", status = HttpStatusCode.OK)
+                    }
+                }
+            }
+        }
+        val testClient = createClient {
+            install(io.ktor.client.plugins.contentnegotiation.ContentNegotiation) { json() }
+        }
+        application {
+            configureSerialization()
+            database = configureDatabases("COLLECTOR_TEST_DB", tables)
+            configureRouting(testClient)
+        }
+        val response = testClient.get("/")
+        assertEquals(HttpStatusCode.OK, response.status)
+        val articles: List<Article> = response.body()
+        assertEquals(TestDoubles.rawArticles, articles)
+
+        transaction(database) {
+            for (table in tables) {
+                    SchemaUtils.drop(table)
+            }
         }
     }
     @Test
-    fun testHealth() = testSuspend {
-        testApp.client.get("/health").apply {
+    fun testHealth() = testApplication {
+        lateinit var database: Database
+        val tables: List<Table> = listOf(RawArticles)
+        externalServices {
+            hosts("https://newsapi.org") {
+                install(ContentNegotiation) { json() }
+                routing {
+                    get("v2/everything{params}") {
+                        call.respond(status = HttpStatusCode.OK, TestDoubles.remoteData)
+                    }
+                }
+            }
+            val apiHost = HostPaths().getAnalyzerPath()
+            hosts("http://${apiHost}") {
+                install(ContentNegotiation) { json() }
+                routing {
+                    post("articles") {
+                        call.respondText("Updated", status = HttpStatusCode.OK)
+                    }
+                }
+            }
+        }
+        val testClient = createClient {
+            install(io.ktor.client.plugins.contentnegotiation.ContentNegotiation) { json() }
+        }
+        application {
+            configureSerialization()
+            database = configureDatabases("COLLECTOR_TEST_DB", tables)
+            configureRouting(testClient)
+        }
+        testClient.get("/health").apply {
             assertEquals(HttpStatusCode.OK, status)
             assertEquals("OK", bodyAsText())
         }
-    }
-
-    @Test
-    fun testRoot() = testSuspend {
-        testApp.client.get("/").apply {
-            assertEquals(HttpStatusCode.OK, status, apiKeyErrorMessage)
-            assertContains(bodyAsText(), "\"publishedAt\":")
+        transaction(database) {
+            for (table in tables) {
+                SchemaUtils.drop(table)
+            }
         }
     }
-    @Test
-    fun testUpdate() = testSuspend {
-        testApp.client.get("/update").apply {
-            assertEquals(HttpStatusCode.OK, status, apiKeyErrorMessage)
-            assertEquals("No update",bodyAsText())
-        }
-    }
-
-
-    companion object {
-        private val testClient = HttpClientTemplate().httpClient
-        lateinit var testApp: TestApplication
-        private lateinit var database: Database
-        private val tables: List<Table> = listOf(RawArticles)
-        @JvmStatic
-        @BeforeClass
-        fun setup() {
-            testApp = TestApplication {
-                externalServices {
-                    hosts("https://newsapi.org") {
-                        install(io.ktor.server.plugins.contentnegotiation.ContentNegotiation) { json() }
-                        routing {
-                            get("v2/everything{params}") {
-                                println("In mock newsapi")
-                                call.respond(status = HttpStatusCode.OK, TestDoubles().remoteData)
-                            }
-                        }
-                    }
-                    val apiHost = HostPaths().getAnalyzerPath()
-                    hosts("http://${apiHost}") {
-                        install(io.ktor.server.plugins.contentnegotiation.ContentNegotiation) { json() }
-                        routing {
-                            post("articles") {
-                                println("In mock analyzer")
-                                call.respondText("Updated", status = HttpStatusCode.OK)
-                            }
-                        }
+    fun testUpdate() = testApplication {
+        lateinit var database: Database
+        val tables: List<Table> = listOf(RawArticles)
+        externalServices {
+            hosts("https://newsapi.org") {
+                install(ContentNegotiation) { json() }
+                routing {
+                    get("v2/everything{params}") {
+                        call.respond(status = HttpStatusCode.OK, TestDoubles.remoteData)
                     }
                 }
-                application {
-                    configureSerialization()
-                    database = configureDatabases("COLLECTOR_TEST_DB")
-                    configureRouting(testClient)
+            }
+            val apiHost = HostPaths().getAnalyzerPath()
+            hosts("http://${apiHost}") {
+                install(ContentNegotiation) { json() }
+                routing {
+                    post("articles") {
+                        call.respondText("Updated", status = HttpStatusCode.OK)
+                    }
                 }
             }
         }
-
-        @JvmStatic
-        @AfterClass
-        fun teardown() {
-            transaction(database) {
-                for (table in tables) {
-                    SchemaUtils.drop(table)
+        val testClient = createClient {
+            install(io.ktor.client.plugins.contentnegotiation.ContentNegotiation) { json() }
+        }
+        application {
+            configureSerialization()
+            database = configureDatabases("COLLECTOR_TEST_DB", tables)
+            configureRouting(testClient)
+        }
+        externalServices {
+            hosts("https://newsapi.org") {
+                install(ContentNegotiation) { json() }
+                routing {
+                    get("v2/everything{params}") {
+                        call.respond(status = HttpStatusCode.OK, TestDoubles.remoteArticlesUpdate)
+                    }
                 }
             }
-            testApp.stop()
+        }
+        val response = testClient.get("update")
+        assertEquals(HttpStatusCode.OK, response.status)
+        val articles: List<Article> = response.body()
+        assertEquals(TestDoubles.updatedRawArticles, articles)
+
+        transaction(database) {
+            for (table in tables) {
+                SchemaUtils.drop(table)
+            }
         }
     }
 }
