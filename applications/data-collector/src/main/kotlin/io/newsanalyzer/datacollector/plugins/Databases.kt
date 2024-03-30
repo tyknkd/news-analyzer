3f2fb9c67df572d07fa4427d85639646d3926197
tyknkd <78797823+tyknkd@users.noreply.github.com>
2024-03-30 16:18:13 +0900

    refactor: add parameters to config db

--- applications/data-collector/src/main/kotlin/io/newsanalyzer/datacollector/plugins/Databases.kt
+++ applications/data-collector/src/main/kotlin/io/newsanalyzer/datacollector/plugins/Databases.kt
@@ -5,9 +5,12 @@ import org.jetbrains.exposed.sql.Table
 import io.newsanalyzer.datasupport.DatabaseTemplate
 import io.newsanalyzer.datasupport.models.RawArticles
 import kotlinx.coroutines.runBlocking
+import org.jetbrains.exposed.sql.Database
 
-fun Application.configureDatabases() {
-    val tables: List<Table> = listOf(RawArticles)
-    DatabaseTemplate("COLLECTOR_DB", tables)
+fun Application.configureDatabases(
+    envDbName: String = "COLLECTOR_DB",
+    tables: List<Table> = listOf(RawArticles)): Database {
+    val database = DatabaseTemplate(envDbName, tables).database
     runBlocking { CollectorDataGateway.updateArticles() }
+    return database
 }
