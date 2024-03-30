f2210236aa0f0435dd5baa3e8f552442563ccdd6
tyknkd <78797823+tyknkd@users.noreply.github.com>
2024-03-31 04:02:51 +0900

    style: remove unused import statements

--- applications/data-analyzer/src/test/kotlin/test/newsanalyzer/dataanalyzer/RawDataGatewayTest.kt
+++ applications/data-analyzer/src/test/kotlin/test/newsanalyzer/dataanalyzer/RawDataGatewayTest.kt
@@ -3,15 +3,7 @@ package test.newsanalyzer.dataanalyzer
 import io.newsanalyzer.dataanalyzer.plugins.*
 import io.newsanalyzer.testsupport.TestDoubles
 import io.newsanalyzer.datasupport.models.*
-import io.newsanalyzer.httpsupport.HostPaths
-import io.ktor.http.*
 import io.ktor.test.dispatcher.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.application.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
-import io.ktor.server.testing.*
 import io.newsanalyzer.datasupport.DatabaseTemplate
 import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
