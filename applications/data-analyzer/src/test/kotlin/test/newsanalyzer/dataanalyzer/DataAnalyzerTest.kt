4b1148c446d623161da3ce9e175b12807c5bb5a1
tyknkd <78797823+tyknkd@users.noreply.github.com>
2024-03-31 05:39:33 +0900

    test: data analyzer

---
+++ applications/data-analyzer/src/test/kotlin/test/newsanalyzer/dataanalyzer/DataAnalyzerTest.kt
@@ -0,0 +1,39 @@
+package test.newsanalyzer.dataanalyzer
+
+import io.newsanalyzer.dataanalyzer.plugins.*
+import io.newsanalyzer.testsupport.TestDoubles
+import io.newsanalyzer.datasupport.models.*
+import io.ktor.test.dispatcher.*
+import io.newsanalyzer.datasupport.DatabaseTemplate
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+import kotlin.test.*
+
+class DataAnalyzerTest {
+    private val tables: List<Table> = listOf(RawArticles)
+    private val database: Database = DatabaseTemplate("ANALYZER_DB", emptyList()).database
+    @BeforeTest
+    fun setup() {
+        transaction(database) {
+            for (table in tables) {
+                SchemaUtils.create(table)
+            }
+        }
+    }
+    @AfterTest
+    fun teardown() {
+        transaction(database) {
+            for (table in tables) {
+                SchemaUtils.drop(table)
+            }
+        }
+    }
+    @Test
+    fun testGetAnalyzedData() = testSuspend {
+        val result = RawDataGateway.upsertArticles(TestDoubles.rawArticles)
+        assertTrue(result)
+        val (articles, topics) = DataAnalyzer.getAnalyzedData()
+        assertEquals(TestDoubles.analyzedArticles, articles)
+        assertEquals(TestDoubles.topics, topics)
+    }
+}
