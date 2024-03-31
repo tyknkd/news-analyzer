c7a6194dd0ecfe75b5c7b0ef410eec24fe0a109b
tyknkd <78797823+tyknkd@users.noreply.github.com>
2024-03-31 19:25:04 +0900

    test: all webserver app routes and pages with test doubles

--- applications/web-server/src/test/kotlin/test/newsanalyzer/webserver/ApplicationTest.kt
+++ applications/web-server/src/test/kotlin/test/newsanalyzer/webserver/ApplicationTest.kt
@@ -1,29 +1,107 @@
 package test.newsanalyzer.webserver
 
+import io.ktor.client.call.*
 import io.newsanalyzer.webserver.plugins.*
+import io.newsanalyzer.webserver.models.*
 import io.newsanalyzer.testsupport.TestDoubles
+import io.newsanalyzer.datasupport.DatabaseTemplate
+import io.newsanalyzer.datasupport.models.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.testing.*
 import io.ktor.test.dispatcher.*
-import io.newsanalyzer.datasupport.models.AnalyzedArticles
-import io.newsanalyzer.datasupport.models.Topics
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.Test
 import org.junit.BeforeClass
 import org.junit.AfterClass
+import org.junit.FixMethodOrder
+import org.junit.runners.MethodSorters
 import kotlin.test.assertContains
 import kotlin.test.assertEquals
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class ApplicationTest {
-
+    @Test
+    fun testAbout() = testSuspend {
+        testClient.get("/about").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            val bodyAsText = bodyAsText()
+            assertContains(bodyAsText,"Tech Industry News Analyzer")
+            assertContains(bodyAsText,"articles")
+            assertContains(bodyAsText,"href=\"/\"")
+        }
+    }
+    @Test
+    fun testApiArticles() = testSuspend {
+        testClient.get("/api/articles").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            val articles: List<Article> = body()
+            assertEquals(TestDoubles.analyzedArticles, articles)
+        }
+    }
+    @Test
+    fun testApiArticlesByTopic() = testSuspend {
+        testClient.get("/api/topics/articles").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            val articlesByTopic: List<ArticlesByTopic> = body()
+            assertEquals(TestDoubles.articlesByTopic, articlesByTopic)
+        }
+    }
+    @Test
+    fun testApiArticlesOnTopic() = testSuspend {
+        testClient.get("/api/topics/0/articles").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            val articlesOnTopic: ArticlesByTopic = body()
+            assertEquals(TestDoubles.articlesOnTopic, articlesOnTopic)
+        }
+    }
+    @Test
+    fun testApiEntrypoint() = testSuspend {
+        testClient.get("/api").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            val endpoint: Endpoint = body()
+            assertEquals(entrypoint, endpoint)
+        }
+    }
+    @Test
+    fun testApiTopics() = testSuspend {
+        testClient.get("/api/topics").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            val topics: List<Topic> = body()
+            assertEquals(TestDoubles.topics, topics)
+        }
+    }
+    @Test
+    fun testApiUpdate() = testSuspend {
+        testClient.post("/api/update") {
+            contentType(ContentType.Application.Json)
+            setBody(TestDoubles.updatedAnalyzedData)
+        }.apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Updated", bodyAsText())
+        }
+        testClient.get("/api/topics/articles").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            val articlesByTopic: List<ArticlesByTopic> = body()
+            assertEquals(TestDoubles.updatedArticlesByTopic, articlesByTopic)
+        }
+    }
+    @Test
+    fun testArticlesOnTopic() = testSuspend {
+        testClient.get("/topics/1/articles").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            val bodyAsText = bodyAsText()
+            assertContains(bodyAsText,"Tech Industry News Analyzer")
+            assertContains(bodyAsText,"Topic Group 1")
+            assertContains(bodyAsText,"href=\"/\"")
+        }
+    }
     @Test
     fun testHealth() = testSuspend {
-        testApp.client.get("/health").apply {
+        testClient.get("/health").apply {
             assertEquals(HttpStatusCode.OK, status)
             assertEquals("OK", bodyAsText())
         }
@@ -31,7 +109,7 @@ class ApplicationTest {
 
     @Test
     fun testRoot() = testSuspend {
-        testApp.client.get("/").apply {
+        testClient.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
             val bodyAsText = bodyAsText()
             assertContains(bodyAsText,"Tech Industry News Analyzer")
@@ -43,7 +121,7 @@ class ApplicationTest {
 
     @Test
     fun testTopics() = testSuspend {
-        testApp.client.get("/topics").apply {
+        testClient.get("/topics").apply {
             assertEquals(HttpStatusCode.OK, status)
             val bodyAsText = bodyAsText()
             assertContains(bodyAsText,"Tech Industry News Analyzer")
@@ -52,95 +130,33 @@ class ApplicationTest {
             assertContains(bodyAsText,"\"/topics/1/articles\"")
         }
     }
-
-    @Test
-    fun testArticlesOnTopic() = testSuspend {
-        testApp.client.get("/topics/1/articles").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            val bodyAsText = bodyAsText()
-            assertContains(bodyAsText,"Tech Industry News Analyzer")
-            assertContains(bodyAsText,"Topic Group 1")
-            assertContains(bodyAsText,"href=\"/\"")
-        }
-    }
-
-    @Test
-    fun testAbout() = testSuspend {
-        testApp.client.get("/about").apply {
-            assertEquals(HttpStatusCode.OK, status)
-        }
-    }
-
-    @Test
-    fun testApiEntrypoint() = testSuspend {
-        testApp.client.get("/api").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            val bodyAsText = bodyAsText()
-            assertContains(bodyAsText,"Tech Industry News Analyzer API")
-            assertContains(bodyAsText,"\"links\":")
-        }
-    }
-
-    @Test
-    fun testApiTopics() = testSuspend {
-        testApp.client.get("/api/topics").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            assertContains(bodyAsText(), "\"terms\":")
-        }
-    }
-    @Test
-    fun testApiArticles() = testSuspend {
-        testApp.client.get("/api/articles").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            assertContains(bodyAsText(), "\"publishedAt\":")
-        }
-    }
-
-    @Test
-    fun testApiArticlesByTopic() = testSuspend {
-        testApp.client.get("/api/topics/articles").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            val bodyAsText = bodyAsText()
-            assertContains(bodyAsText, "\"topic\":")
-            assertContains(bodyAsText, "\"articles\":")
-            assertContains(bodyAsText, "\"terms\":")
-            assertContains(bodyAsText, "\"publishedAt\":")
-        }
-    }
-
-    @Test
-    fun testApiArticlesOnTopic() = testSuspend {
-        testApp.client.get("/api/topics/1/articles").apply {
-            assertEquals(HttpStatusCode.OK, status)
-            val bodyAsText = bodyAsText()
-            assertContains(bodyAsText, "\"topic\":")
-            assertContains(bodyAsText, "\"topicId\": 1")
-            assertContains(bodyAsText, "\"articles\":")
-            assertContains(bodyAsText, "\"terms\":")
-            assertContains(bodyAsText, "\"publishedAt\":")
-        }
-    }
-
     companion object {
-        lateinit var testApp: TestApplication
-        private lateinit var database: Database
         private val tables: List<Table> = listOf(AnalyzedArticles, Topics)
-        @JvmStatic
-        @BeforeClass
-        fun setup(): Unit {
-            testApp = TestApplication {
-                application {
-                    configureMonitoring()
-                    configureSerialization()
-                    database = configureDatabases("WEBSERVER_TEST_DB", tables)
-                    configureTemplating()
-                    configureRouting()
-                }
+        private val database: Database = DatabaseTemplate("WEBSERVER_TEST_DB", emptyList()).database
+        private val testApp = TestApplication {
+            application {
+                configureSerialization()
+                configureTemplating()
+                configureRouting()
             }
         }
-
+        private val testClient = testApp.createClient {
+            install(io.ktor.client.plugins.contentnegotiation.ContentNegotiation) { json() }
+        }
+        @BeforeClass
         @JvmStatic
+        fun setup(): Unit {
+            transaction(database) {
+                for (table in tables) {
+                    SchemaUtils.create(table)
+                }
+            }
+            testSuspend {
+                WebDataGateway.updateAll(TestDoubles.analyzedArticles,TestDoubles.topics)
+            }
+        }
         @AfterClass
+        @JvmStatic
         fun teardown() {
             transaction(database) {
                 for (table in tables) {
