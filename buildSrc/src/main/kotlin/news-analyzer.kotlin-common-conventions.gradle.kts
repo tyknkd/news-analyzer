9997bbb0eddd3349a63e893d465620d9d0def3c7
tyknkd <78797823+tyknkd@users.noreply.github.com>
2024-04-22 09:15:11 +0900

    build: add kotlinx-coroutines-test

--- buildSrc/src/main/kotlin/news-analyzer.kotlin-common-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/news-analyzer.kotlin-common-conventions.gradle.kts
@@ -18,12 +18,14 @@ repositories {
 
 val ktor_version: String by project
 val kotlin_version: String by project
+val kotlinx_coroutines_version: String by project
 val logback_version: String by project
 
 dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0-RC.2")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$kotlinx_coroutines_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
 
