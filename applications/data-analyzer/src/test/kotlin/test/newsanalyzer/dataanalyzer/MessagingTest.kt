049e5f43de24acef10c18cb68ea5126cca9d7d05
tyknkd <78797823+tyknkd@users.noreply.github.com>
2024-04-23 16:48:57 +0900

    test: mq

---
+++ applications/data-analyzer/src/test/kotlin/test/newsanalyzer/dataanalyzer/MessagingTest.kt
@@ -0,0 +1,69 @@
+package test.newsanalyzer.dataanalyzer
+
+import io.newsanalyzer.dataanalyzer.plugins.Messaging
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import org.junit.BeforeClass
+import org.junit.AfterClass
+import kotlin.test.assertEquals
+
+class MessagingTest {
+    @Test
+    fun testCollectorMq() = runTest {
+        val testMessage = "correct collector message"
+        launch {
+
+            Messaging.collectorMessenger.publishMessage(testMessage)
+        }
+        launch {
+            assertEquals(testMessage, collectorMqPublished)
+        }
+    }
+    @Test
+    fun testAnalyzerMq() = runTest {
+        val testMessage = "correct analyzer message"
+        launch {
+            Messaging.analyzerMessenger.publishMessage(testMessage)
+        }
+        launch {
+            assertEquals(testMessage, analyzerMqPublished)
+        }
+    }
+    companion object {
+        private var collectorMqPublished = "wrong collector message"
+        private var analyzerMqPublished = "wrong analyzer message"
+        private fun collectorMessageHandler(message: String): Boolean {
+            collectorMqPublished = message
+            return true
+        }
+        private fun analyzerMessageHandler(message: String): Boolean {
+            analyzerMqPublished = message
+            return true
+        }
+        @BeforeClass
+        @JvmStatic
+        fun setup() {
+            Messaging.updateCollectorMessenger(
+                exchangeName = "analyzer_app_test_collector_exchange",
+                queueName = "analyzer_app_test_collector_queue",
+                routingKey = "analyzer_app_test_collector_key",
+                messageHandler = ::collectorMessageHandler
+            )
+            Messaging.collectorMessenger.listen()
+            Messaging.updateAnalyzerMessenger(
+                exchangeName = "analyzer_app_test_analyzer_exchange",
+                queueName = "analyzer_app_test_analyzer_queue",
+                routingKey = "analyzer_app_test_analyzer_key",
+                messageHandler = ::analyzerMessageHandler
+            )
+            Messaging.analyzerMessenger.listen()
+        }
+        @AfterClass
+        @JvmStatic
+        fun teardown() {
+            Messaging.collectorMessenger.delete()
+            Messaging.analyzerMessenger.delete()
+        }
+    }
+}
